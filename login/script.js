1b3c6a47bb9ced15c7a91f703d943e28094d5d6c
Alexkauann17 <alexkauan00001@icloud.com>
2024-11-13 18:01:41 -0300

    site

--- login/script.js
+++ login/script.js
@@ -3,7 +3,7 @@ function finalizar() {
     var senha = document.getElementById('senha')
     var fina = document.getElementById('finalizar')
     if (nome.value == `Emily` && senha.value == 3006) {
-        window.open('https://www.youtube.com/watch?v=Ao81ziiXHhs','_blank')
+        window.open('../home/index.html','_blank')
 
     } else {
         window.alert('[ERRO] SENHA OU USUARIO INVALIDA')
