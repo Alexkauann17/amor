f7deddfad5a79c822c96634628bb9616204a436d
Alexkauann17 <alexkauan00001@icloud.com>
2024-11-14 10:39:53 -0300

    site

--- login/script.js
+++ login/script.js
@@ -2,7 +2,8 @@ function finalizar() {
     var nome = document.getElementById('nome')
     var senha = document.getElementById('senha')
     var fina = document.getElementById('finalizar')
-    if (nome.value == `Emily` && senha.value == 3006) {
+    var se = String(senha.value)
+    if (nome.value == `moletom cinza` && se == "0308") {
         window.open('../home/index.html','_blank')
 
     } else {
